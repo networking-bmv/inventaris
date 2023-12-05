be3e3dc5912353830da49f1b6042cc0904b2bb0d
Vito Karyadi <77707067+VitoKaryadi@users.noreply.github.com>
2023-12-05 23:16:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,4 @@ $DATABASE_NAME = 'inventaris_audiovisual';
 
 $db = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-if( !$db ){
-    die("failed to connect database!: " . mysqli_connect_error());
-}
-
 ?>
